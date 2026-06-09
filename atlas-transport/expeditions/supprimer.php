58aea38401cb2decafd37713147945d5731d28e8
elferighesniserinsolicode-maker <elferighes.niserin.solicode@gmail.com>
2026-06-09 14:10:09 +0000

    Delete atlas-transport directory

--- atlas-transport/expeditions/supprimer.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if (!isset($_SESSION['admin'])) { header("Location: ../connexion.php"); exit; }
-require '../db.php';
-$pdo->prepare("DELETE FROM expeditions WHERE id=?")->execute([intval($_GET['id'] ?? 0)]);
-header("Location: liste.php?success=1");
-exit;
-?>
